fa8a2f82d53e2879acc982eb6b2e42d6abcd103d
Greg Thomas <gthoma17@emich.edu>
2021-05-21 12:02:17 -0600

    WIP

--- dockerfile
+++ dockerfile
@@ -2,53 +2,5 @@ FROM debian:buster-slim
 
 # Adapted from https://github.com/tentone/tello-ros2
 
-
-RUN apt-get update &&\
-	apt update
-
-# C++ Build tools
-RUN apt install -y build-essential gdb
-
-# Set UTF-8 charset
-RUN apt install -y locales &&\
-	locale-gen en_US en_US.UTF-8 &&\
-	update-locale LANG=en_US.UTF-8 &&\
-	update-locale LC_ALL=en_US.UTF-8 &&\
-	export LANG=en_US.UTF-8
-
-# Add ROS2 sources
-RUN apt update &&\
-	apt install -y curl gnupg2 lsb-release &&\
-	curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | apt-key add - &&\
-	sh -c 'echo "deb [arch=$(dpkg --print-architecture)] http://packages.ros.org/ros2/ubuntu $(lsb_release -cs) main" > /etc/apt/sources.list.d/ros2-latest.list'
-
-# Install ROS 2 (foxy)
-RUN apt-get update &&\
-	apt update &&\
-	apt install -y ros-foxy-desktop
-
-# Step envrioment
-RUN source /opt/ros/foxy/setup.bash
-
-# Argcomplete
-RUN apt install -y python3-pip &&\
-	pip3 install -U argcomplete
-
-# Colcon build tools
-RUN apt install -y python3-colcon-common-extensions python3-rosdep2
-
-# Update ROS dep
-RUN rosdep update &&\
-	rosdep fix-permissions
-
-# Add to bashrc
-RUN echo "source /opt/ros/foxy/setup.bash" >> ~/.bashrc &&\
-	echo "source /usr/share/colcon_cd/function/colcon_cd.sh" >> ~/.bashrc &&\
-	echo "export _colcon_cd_root=~/ros2_install" >> ~/.bashrc &&\
-	source ~/.bashrc
-
-# Install project dependencies
-RUN pip3 install catkin_pkg rospkg av image opencv-python djitellopy2 pyyaml &&\
-	apt install -y python3-tf* &&\
-	apt install -y ros-foxy-ament-cmake* ros-foxy-tf2* ros-foxy-rclcpp* ros-foxy-rosgraph* &&\
-	apt install -y ros-foxy-rviz* ros-foxy-rqt*
+COPY scripts/install.sh /bootstrap.sh
+RUN ["/bin/bash", "-c", "bash bootstrap.sh"]
--- scripts/install.sh
+++ scripts/install.sh
@@ -1,26 +1,40 @@
 #!/bin/bash
-set -xe
+set -e
 
 permissionsCheck () {
-	if [ $EUID > 0 ] && ![ -f /.dockerenv ]; then
+	echo "***************************"
+	echo " Entering $FUNCNAME "
+	echo "***************************"
+	# Ensures user is either root, or we're running inside Docker
+	if [ $EUID > 0 ] && ! [ -f /.dockerenv ]; then
 		echo " - Please run as root"
 		exit
 	fi
 }
 
 installCppBuildTools () {
-	apt install build-essential gdb
+	echo "***************************"
+	echo " Entering $FUNCNAME "
+	echo "***************************"
+	apt update
+	apt install -y build-essential gdb
 }
 
 setLocales () {
+	echo "***************************"
+	echo " Entering $FUNCNAME "
+	echo "***************************"
 	apt update
-	apt install -y locales
+	apt install -y locales locales-all # need all, otherwise the container errors `update-locale: Error: invalid locale settings:  LANG=en_US.UTF-8 LC_ALL=en_US.UTF-8`
 	locale-gen en_US en_US.UTF-8
 	update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
 	export LANG=en_US.UTF-8
 }
 
 addROS2SourcesToApt () {
+	echo "***************************"
+	echo " Entering $FUNCNAME "
+	echo "***************************"
 	apt update
 	apt install -y curl gnupg2 lsb-release
 	curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | apt-key add -
@@ -28,26 +42,41 @@ addROS2SourcesToApt () {
 }
 
 installROS2Foxy () {
+	echo "***************************"
+	echo " Entering $FUNCNAME "
+	echo "***************************"
 	apt update
 	apt install -y ros-foxy-desktop
 }
 
 setupEnvironment () {
+	echo "***************************"
+	echo " Entering $FUNCNAME "
+	echo "***************************"
 	source /opt/ros/foxy/setup.bash
 }
 
 installPythonBuildTools () {
+	echo "***************************"
+	echo " Entering $FUNCNAME "
+	echo "***************************"
 	apt install -y python3-pip python3-colcon-common-extensions python3-rosdep2
 	pip3 install -U argcomplete
 }
 
 setupRosdep () {
+	echo "***************************"
+	echo " Entering $FUNCNAME "
+	echo "***************************"
 	rosdep init
 	rosdep update
 	rosdep fix-permissions
 }
 
 addToBashrc () {
+	echo "***************************"
+	echo " Entering $FUNCNAME "
+	echo "***************************"
 	echo "source /opt/ros/foxy/setup.bash" >> ~/.bashrc
 	echo "source /usr/share/colcon_cd/function/colcon_cd.sh" >> ~/.bashrc
 	echo "export _colcon_cd_root=~/ros2_install" >> ~/.bashrc
@@ -55,6 +84,9 @@ addToBashrc () {
 }
 
 installProjectDeps () {
+	echo "***************************"
+	echo " Entering $FUNCNAME "
+	echo "***************************"
 	pip3 install catkin_pkg rospkg av image opencv-python djitellopy2 pyyaml
 	apt install python3-tf*
 
@@ -66,10 +98,13 @@ installProjectDeps () {
 }
 
 main () {
+	echo "***************************"
+	echo " Entering $FUNCNAME "
+	echo "***************************"
 	permissionsCheck
 	installCppBuildTools
 	setLocales
-	addROS2Sources
+	addROS2SourcesToApt
 	installROS2Foxy
 	setupEnvironment
 	installPythonBuildTools
@@ -77,3 +112,5 @@ main () {
 	addToBashrc
 	installProjectDeps
 }
+
+main
